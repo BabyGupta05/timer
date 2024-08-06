d5d6f375f9f9cdd868e92f517f7d3e1d876c7510
BabyGupta05 <codecode355@gmail.com>
2024-08-07 00:08:21 +0530

    added mui component in timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,66 @@
-import { useCallback } from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { MultiSectionDigitalClock } from "@mui/x-date-pickers/MultiSectionDigitalClock";
+import dayjs from "dayjs";
 
 function App() {
-  const [time, setTime] = useState(10);
+  const [selectedTime, setSelectedTime] = useState(
+    dayjs().hour(0).minute(0).second(0)
+  );
+  const [initialTime, setInitialTime] = useState(
+    dayjs().hour(0).minute(0).second(0)
+  );
   const [running, setRunning] = useState(false);
 
   useEffect(() => {
     let id;
-    if (running && time > 0) {
+    if (running && selectedTime.diff(dayjs().startOf("day"), "second") > 0) {
       id = setInterval(() => {
-        setTime((prevTime) => prevTime - 1);
+        setSelectedTime((prevTime) => prevTime.subtract(1, "second"));
       }, 1000);
-    }
-    if (time <= 0) {
+    } else if (selectedTime.diff(dayjs().startOf("day"), "second") <= 0) {
       setRunning(false);
     }
     return () => clearInterval(id);
-  }, [running, time]);
+  }, [running, selectedTime]);
 
   const handleStart = () => {
-    if (!running) {
+    if (!running && selectedTime.diff(dayjs().startOf("day"), "second") > 0) {
       setRunning(true);
     }
   };
 
   const handleReset = useCallback(() => {
-    if (running) {
-      setRunning(false);
-    }
-    setTime(10);
-  }, [running]);
+    setRunning(false);
+    setSelectedTime(initialTime);
+  }, [initialTime]);
 
   const handleStop = useCallback(() => {
-    if (running) {
-      setRunning(false);
-    }
-  }, [running]);
+    setRunning(false);
+  }, []);
+
+  const handleTimeChange = (newTime) => {
+      setSelectedTime(newTime);
+      setInitialTime(newTime);
+      setRunning(false)
+  };
 
   return (
-    <div>
-      <h1 className="font-bold text-9xl">{time}</h1>
+    <div className="grid place-items-center h-screen bg-blue-100">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={["MultiSectionDigitalClock"]}>
+          <MultiSectionDigitalClock
+            timeSteps={{ hours: 1, minutes: 1, seconds: 1 }}
+            views={["hours", "minutes", "seconds"]}
+            value={initialTime}
+            onChange={handleTimeChange}
+            ampm={false}
+          />
+        </DemoContainer>
+      </LocalizationProvider>
+      <h1 className="font-bold text-9xl">{selectedTime.format("HH:mm:ss")}</h1>
       <div className="flex gap-10">
         <button onClick={handleStart}>start</button>
         <button onClick={handleReset}>reset</button>
